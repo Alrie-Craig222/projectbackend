869af1e775e98f94047b62c9d736b3199c39f160
Alrie-Craig222 <almariemaestrado05@gmail.com>
2024-03-18 15:31:48 +0800

    Added Logout API and Correct Route Groups

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\vehiclelistController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\vehiclelistController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,23 +16,61 @@ use App\Http\Controllers\Api\UserController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Public APIs
+Route::post('login', [AuthController::class, 'login'])->name('user.login');
+// Route::post('user', [UserController::class, 'store'])->name('user.store');
+
+//Private APIs
+Route::middleware(['auth:sanctum'])->group(function () {
+Route::post('/logout', [AuthController::class, 'logout']);
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::middleware(['auth:sanctum'])->group(function () {
+// 	Route::post('/logout', [AuthController::class, 'logout']);
+
+// Route::controller(AuthController::class)->group(function (){
+// 	Route::post('/login', 'login')->name('user.login');
+// 	Route::post('/logout', 'logout');
+// });
+
+Route::post('/login', [AuthController::class, 'login'])->name('user.login')->name('user.login');
+Route::post('/user',	[UserController::class, 'store'])->name('user.store');
+
+
+Route::controller(UserController::class)->group(function (){
+	Route::get('/user',									'index');
+	Route::get('/user/{id}',						  	'show');
+	Route::put('/user/{id}', 'update')->name(		  	'user.update');
+	Route::put('/user/email/{id}',	'email')->name(	  	'user.email');
+	Route::put('/user/password{id}','password')->name(	'user.password');
+	Route::delete('/user/{id}',							'destroy');
 });
+	
+Route::controller(vehiclelistController::class)->group(function (){
+	Route::get('/vehicle', 			'index');
+	Route::get('/vehicle/{id}', 	'show');
+	Route::post('/vehicle', 		'store');
+	Route::put('/vehicle/{id}', 	'update');
+	Route::delete('/vehicle/{id}', 	'destroy');
+});
+// Route::get('/vehicle',[vehiclelistController::class, 'index']);
+// Route::get('/vehicle/{id}', [vehiclelistController::class, 'show']);
+// Route::delete('/vehicle/{id}', [vehiclelistController::class, 'destroy']);
+// Route::post('/vehicle',[vehiclelistController::class, 'store']);
+// Route::put('/vehicle/{id}',[vehiclelistController::class, 'update']);
 
-Route::get('/vehicle',[vehiclelistController::class, 'index']);
-Route::get('/vehicle/{id}', [vehiclelistController::class, 'show']);
-Route::delete('/vehicle/{id}', [vehiclelistController::class, 'destroy']);
-Route::post('/vehicle',[vehiclelistController::class, 'store']);
-Route::put('/vehicle/{id}',[vehiclelistController::class, 'update']);
+// Route::get('/user',[UserController::class, 'index']);
+// Route::DELETE('/user/{id}',[UserController::class, 'destroy']);
+// Route::get('/user/{id}',[UserController::class, 'show']);
+// Route::post('/user',[UserController::class, 'store']);
+// Route::put('/user/{id}',[UserController::class, 'update']);
 
-Route::get('/user',[UserController::class, 'index']);
-Route::DELETE('/user/{id}',[UserController::class, 'destroy']);
-Route::get('/user/{id}',[UserController::class, 'show']);
-Route::post('/user',[UserController::class, 'store']);
-Route::put('/user/{id}',[UserController::class, 'update']);
-
-Route::post('/user',[UserController::class, 'store'])->name('user.store'); 
-Route::put('/user/{id}',[UserController::class, 'update'])->name('user.update');
-Route::put('/user/email/{id}',[UserController::class, 'email'])->name('user.email');
-Route::put('/user/password/{id}',[UserController::class, 'password'])->name('user.password');
+// Route::post('/user',[UserController::class, 'store'])->name('user.store'); 
+// Route::put('/user/{id}',[UserController::class, 'update'])->name('user.update');
+// Route::put('/user/email/{id}',[UserController::class, 'email'])->name('user.email');
+// Route::put('/user/password/{id}',[UserController::class, 'password'])->name('user.password');
+});
